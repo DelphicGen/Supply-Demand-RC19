62c38b45131a9606464b194931ca63b59d4712cb
rayhannr <rayhannaufal2017@mail.ugm.ac.id>
2020-04-01 23:06:49 +0700

    benerin sidebar dikit

--- frontend/src/components/Dashboard/SideBar.js
+++ frontend/src/components/Dashboard/SideBar.js
@@ -4,7 +4,7 @@ import Link from './Link'
 
 const Sidebar = props => {
     return (
-        <div className="w-full md:w-1/4 lg:w-1/5 bg-blue-800 rounded-t-lg md:rounded-t-none md:rounded-r-lg md:bg-blue-800 text-center fixed bottom-0 md:pt-10 md:top-0 md:left-0 h-16 md:h-screen">
+        <div className="w-full md:w-1/4 xl:w-1/5 bg-blue-800 rounded-t-lg md:rounded-t-none md:rounded-r-lg md:bg-blue-800 text-center fixed bottom-0 md:pt-10 md:top-0 md:left-0 h-16 md:h-screen">
             <div className="hidden md:block md:text-left lg:pl-5 md:pl-3">
                 <h5 className="text-white lg:text-base font-semibold text-xs">{`Dashboard ${props.role}`} </h5>
                 <h2 className="text-white lg:text-4xl font-semibold text-2xl">{props.name}</h2>
