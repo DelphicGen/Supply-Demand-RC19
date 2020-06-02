f1f44fab0d589715bdcd36fc3b5bf8026ba96019
rayhannr <rayhannr07@gmail.com>
2020-06-02 23:06:59 +0700

    fixed button issue

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import {useMediaQuery} from '../../hooks/medquery-hook'
+import { Link } from 'react-router-dom'
+import { useMediaQuery } from '../../hooks/medquery-hook'
 
 const Button = props => {
     const mediaQuery = useMediaQuery('(max-width: 419px)')
@@ -11,21 +11,32 @@ const Button = props => {
             maxHeight: 41
         })
     }
-    if(props.to || props.scroll){
+    if (props.to) {
         return (
-            <Link 
-                to={props.to} 
+            <Link
+                to={props.to}
                 className="px-4 py-2 bg-blue-800 rounded-md focus:outline-none text-gray-200 mr-2 text-xs font-semibold tracking-widest transform hover:scale-95 hover:shadow-md duration-300"
-                style={!props.width ? styles.container(mediaQuery) : {width: props.width}} 
+                style={!props.width ? styles.container(mediaQuery) : { width: props.width }}
             >
                 {props.children}
             </Link>
         )
     }
-    return(
+
+    if (props.scroll) {
+        return (
+            <span
+                className="inline px-4 py-2 bg-blue-800 rounded-md focus:outline-none text-gray-200 mr-2 text-xs font-semibold tracking-widest transform hover:scale-95 hover:shadow-md duration-300"
+                style={!props.width ? styles.container(mediaQuery) : { width: props.width }}
+            >
+                {props.children}
+            </span>
+        )
+    }
+    return (
         <button
             className={`mt-8 p-2 bg-blue-800 rounded-md flex items-center justify-center focus:outline-none shadow-xl text-gray-200 text-lg font-bold tracking-widest duration-300 ${props.disabled ? 'cursor-not-allowed opacity-50 text-white' : 'hover:bg-indigo-900 focus:bg-purple-900'}`}
-            style={{width: props.width, maxHeight: 41}} 
+            style={{ width: props.width, maxHeight: 41 }}
             onClick={props.onClick}
             type={props.type}
             disabled={props.disabled}
