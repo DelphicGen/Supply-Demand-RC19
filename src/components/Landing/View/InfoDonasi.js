e87ba2a3f9c3c62fbac952a73c8d93690961e59c
DelphicGen <gennardokidam@gmail.com>
2020-06-26 11:00:33 +0700

    Tambahan 2 poin pertama bu yunita

--- src/components/Landing/View/InfoDonasi.js
+++ src/components/Landing/View/InfoDonasi.js
@@ -9,9 +9,10 @@ const InfoDonasi = () => (
     <div className="bg-gray-200 py-16">
         <h3 className="text-center text-blue-800 text-xl md:text-2xl font-bold mb-4">Info Donasi</h3>
         <p className="text-center text-gray-800 text-xs md:text-sm leading-relaxed max-w-xs sm:max-w-sm md:max-w-xl lg:max-w-2xl px-4 mx-auto mb-8">
-            SONJO Husada membantu pengadaan logistik medis dan non medis. Donasi yang terkumpul melalui gerakan ini di
+            {/* SONJO Husada membantu pengadaan logistik medis dan non medis. Donasi yang terkumpul melalui gerakan ini di
             lembaga-lembaga kemanusiaan di bawah akan disalurkan langsung ke RS dan Puskesmas untuk logistik Fasyankes
-            di DIY. SONJO Husada juga menerima sumbangan dalam bentuk barang yang bisa dikoordinasikan bersama.
+            di DIY. SONJO Husada juga menerima sumbangan dalam bentuk barang yang bisa dikoordinasikan bersama. */}
+            SONJO Husada adalah program kerjasama antara Sambatan Jogja (SONJO) dan Tim Relawan Covid 19. Program ini bertujuan untuk menyalurkan bantuan logistik ke rumah sakit, puskesmas, klinik (fasilitas layanan kesehatan)  di Daerah Istimewa Yogyakarta dalam menghadapi Covid-19. Pemenuhan  logistik ditujukan untuk semua yg berjuang  di garda depan para Tenaga Kesehatan, Pasien dan Keluarga. Untuk mendukung program SONJO Husada, situs <strong>Peduli Corona</strong> dibangun untuk mempermudah donatur/ masyarakat yang peduli untuk meringankan dengan memenuhi kebutuhan logistik medis & non-medis.Situs ini dikembangkan oleh Tim Relawan Covid 19 dan gerakan Sambatan Jogja (SONJO).
         </p>
 
         <BigCard title="Langkah Donasi Barang dan Alat Kesehatan">
@@ -31,6 +32,12 @@ const InfoDonasi = () => (
             <BigCardList number="4">
                 Pilih menu <span className="font-semibold">‘Alokasi Bantuan’</span>, kemudian pilih permohonan bantuan yang akan dibantu. Setelah memilih, alokasikan donasi berupa barang yang sudah berlabel 'Ready'
             </BigCardList>
+
+            <BigCardList number="5">
+                Jika membutuhkan dokumen bukti penyerahan bisa didownload di link berikut <a href="shorturl.at/uxCWY"><strong>shorturl.at/uxCWY. </strong></a>
+                Dokumen yang sudah diisi dan foto bukti donasi kemudian bisa diunggah melalui <a href="shorturl.at/hyFVW"><strong>shorturl.at/hyFVW</strong></a> atau dikirimkan ke e-mail 
+                <strong> sonjo.supply.demand@gmail.com</strong>
+            </BigCardList>
         </BigCard>
 
         <BigCard title="Langkah Donasi Dana">
@@ -41,7 +48,12 @@ const InfoDonasi = () => (
                 Transfer ke nomor rekening lembaga pilihan Anda. Untuk memudahkan administrasi, setiap transaksi untuk
                 kegiatan ini ditambahkan angka 123. Misal Anda ingin mendonasikan Rp50.000, tambahkan 123 di akhir
                 sehingga menjadi Rp50.123
-                </BigCardList>
+            </BigCardList>
+            <BigCardList number="3">
+                Jika membutuhkan dokumen bukti penyerahan bisa didownload di link berikut <a href="shorturl.at/uxCWY"><strong>shorturl.at/uxCWY. </strong></a>
+                Dokumen yang sudah diisi dan foto bukti donasi kemudian bisa diunggah melalui <a href="shorturl.at/hyFVW"><strong>shorturl.at/hyFVW</strong></a> atau dikirimkan ke e-mail 
+                <strong> sonjo.supply.demand@gmail.com</strong>
+            </BigCardList>
         </BigCard>
 
         <h3 className="text-center text-blue-800 text-sm md:text-lg font-bold mb-6 mt-20">Daftar Lembaga Sosial</h3>
